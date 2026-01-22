7e063cc6a670b4f8d5527cd7b8ad23b2d500c3ca
KaraEngelhardt <86666034+KaraEngelhardt@users.noreply.github.com>
2026-01-22 13:05:07 +0100

    Add files via upload

---
+++ reef_metabolites/read_me.R
@@ -0,0 +1,14 @@
+# R version 4.3.0
+# this script belongs to the paper "Biodiversity affects the exometabolomes of four benthic functional groups in coral reefs"
+# by Engelhardt et al., 2024
+
+# order of scripts
+
+# 1.) read_me
+# 2.) data preparation -> includes bucket table preparation (M2NT) & curated feature data (CFT)
+# 3.) statistical analysis -> glmer with buket table and curated feature data & 
+#     indicator analysis with curated feature table (including heatmap)
+# 4.) Graphics -> all graphs (except heatmaps) found in the manuscript and supplementary information
+
+# Raw tables contain the abbreviation Sin (Sinularia sp.), which has been replaced by 
+# Scl (Sclerophytum wanannense) in the manuscript and graphics.
